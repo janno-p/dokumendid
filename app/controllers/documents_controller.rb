0221009e09b2eaa136ee6d83ddcbc41b808e454b
Janno Põldma <janno.poldma@gmail.com>
2014-06-07 16:08:45 +0300

    DRY-ed out documents controller.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,27 +1,21 @@
 class DocumentsController < ApplicationController
+  before_filter :load_catalog_context, except: [:attributes]
+  before_filter :load_document_context, only: [:new, :edit]
+
   def index
-    @root = DocCatalog.root
-    @current_catalog = params[:catalog_id].to_i == 0 ? @root : DocCatalog.find(params[:catalog_id])
   end
 
   def new
     if params[:catalog_id].to_i == 0 then
       redirect_to catalog_documents_path(0)
     else
-      @root = DocCatalog.root
       @document = Document.new
-      @current_catalog = params[:catalog_id].to_i == 0 ? @root : DocCatalog.find(params[:catalog_id])
-      @document_types = DocType.joins(:doc_types).where('"doc_type"."level" = 1').uniq.all
-      @doc_status_types = DocStatusType.order(:type_name).all
     end
   end
 
   def edit
-    @root = DocCatalog.root
     @document = Document.find(params[:id])
     @current_catalog = @document.doc_catalog
-    @document_types = DocType.joins(:doc_types).where('"doc_type"."level" = 1').uniq.all
-    @doc_status_types = DocStatusType.order(:type_name).all
   end
 
   def attributes
@@ -30,5 +24,17 @@ class DocumentsController < ApplicationController
     else
       not_found
     end
+  end
+
+  private
+
+  def load_catalog_context
+    @root = DocCatalog.root
+    @current_catalog = params[:catalog_id].to_i == 0 ? @root : DocCatalog.find(params[:catalog_id])
+  end
+
+  def load_document_context
+    @document_types = DocType.joins(:doc_types).where('"doc_type"."level" = 1').uniq.all
+    @doc_status_types = DocStatusType.order(:type_name).all
   end
 end
